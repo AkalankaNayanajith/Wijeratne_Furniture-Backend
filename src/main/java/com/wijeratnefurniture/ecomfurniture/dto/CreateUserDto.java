b46d41a1a3c32ee8392b71296ab6ea5c6fed17ca
AkalankaNayanajit <123anjulaakalankanayanajith@gmail.com>
2022-12-06 10:40:23 +0530

    a user can be added successfully

--- src/main/java/com/wijeratnefurniture/ecomfurniture/dto/CreateUserDto.java
+++ src/main/java/com/wijeratnefurniture/ecomfurniture/dto/CreateUserDto.java
@@ -9,16 +9,17 @@ import lombok.*;
 @Getter
 @Setter
 public class CreateUserDto {
-
-   
+ 
+    private String userid; //we give ID manually
     private String firstName; 
     private String lastName; 
     private String address; 
     private String gender; 
     private String NIC;
     private String role;
-    private String userName;      
+    private String email;      
     private String password;
+    private String image64;
 // image to be added
     private float age;
     private String DOB;
